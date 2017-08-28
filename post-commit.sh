dc4ad17a35aba860a4ee2bcc39d30344acd724a3
a.quintero10 <a.quintero10@ISIS2603S4-0049.sis.virtual.uniandes.edu.co>
2017-08-28 13:30:17 -0500

    se remueven unused fields

--- post-commit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
- 
-
-curl http://localhost:8080/git/notifyCommit?url=git@github.com:Uniandes-isis2603/wowCars.git
--- post-receive.bat
+++
@@ -1,6 +0,0 @@
-
- echo "started"
-
-WGET64 http://localhost:8080/git/notifyCommit?url=git@github.com:Uniandes-isis2603/wowCars.git
-
-echo "finishid"

1b9eaed6a1a580a7db9c296b125d6717477cb655
a.quintero10 <a.quintero10@ISIS2603S4-0049.sis.virtual.uniandes.edu.co>
2017-08-28 13:25:44 -0500

    test

---
+++ post-commit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ 
+
+curl http://localhost:8080/git/notifyCommit?url=git@github.com:Uniandes-isis2603/wowCars.git
---
+++ post-receive.bat
@@ -0,0 +1,6 @@
+
+ echo "started"
+
+WGET64 http://localhost:8080/git/notifyCommit?url=git@github.com:Uniandes-isis2603/wowCars.git
+
+echo "finishid"
